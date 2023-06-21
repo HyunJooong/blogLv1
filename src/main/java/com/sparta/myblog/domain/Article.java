baa35d9fc9d4545cc1a80881c527bf7dbba3c745
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-06-21 20:38:55 +0900

    블로그 게시글 목록 조회

---
+++ src/main/java/com/sparta/myblog/domain/Article.java
@@ -0,0 +1,33 @@
+package com.sparta.myblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id //primaryKey setting??
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder //builder annotation from lombok
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+
+    }
+
+
+}
---
+++ src/main/java/com/sparta/myblog/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package com.sparta.myblog.dto;
+
+import com.sparta.myblog.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article){
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
