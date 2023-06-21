882cb78a71f1a5b6df15251e63efd65d2ad2f07a
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-06-21 21:06:43 +0900

    다시 원점으로 돌아가기

--- src/main/java/com/sparta/myblog/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.myblog.repository;
-
-import com.sparta.myblog.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-    
-}
