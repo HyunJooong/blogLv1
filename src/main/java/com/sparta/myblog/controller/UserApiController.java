1cac21b06aedda2416d7505d72dc508c8b672acb
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-07-01 16:50:16 +0900

    회원가입,로그인,로그아웃 개발

--- src/main/java/com/sparta/myblog/controller/UserApiController.java
+++ src/main/java/com/sparta/myblog/controller/UserApiController.java
@@ -2,8 +2,14 @@ package com.sparta.myblog.controller;
 
 import com.sparta.myblog.dto.AddUserRequest;
 import com.sparta.myblog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,6 +22,14 @@ public class UserApiController {
     public String signup(AddUserRequest addUserRequest){
         userService.save(addUserRequest); //회원 가입 메서드 추출
         return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response,
+                SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:login";
     }
 
 }
